282d3a7622e8b66bd3e28e53a39b69883351352f
SergeyZet <nikjoynik@gmail.com>
2020-02-06 15:45:23 +0600

    dell src))

--- src/js/import/components.js
+++
@@ -1,1 +0,0 @@
-import "../../blocks/components/burger/burger.js";
--- src/js/index.js
+++
@@ -1,8 +0,0 @@
-import "./import/modules.js";
-import "./import/components.js";
-import $ from 'jquery';
-import svg4everybody from 'svg4everybody';
-
-$(document).ready(function () {
-	svg4everybody();
-})
